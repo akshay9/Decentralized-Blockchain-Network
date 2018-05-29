3a2dee4453e4cc3a12971c5965746a8cded44459
hi_man_shoe <hello2himanshusingh@gmail.com>
2018-05-30 02:00:35 +0530

    Testing: CreateNewTransaction

--- dev/test.js
+++ dev/test.js
@@ -4,8 +4,12 @@ const bitcoin = new Blockchain();
 /**
  * Testing the create block method with dummy data-set
  */
-bitcoin.createNewBlock(222,'prev1','hash1')
-bitcoin.createNewBlock(23332,'prev2','hash2')
-bitcoin.createNewBlock(222222,'prev3','hash3')
+bitcoin.createNewBlock(222, 'prev1', 'hash1')
+bitcoin.createNewTransaction(100, "Sender1", "Recipient1");
+bitcoin.createNewBlock(200, 'prev2', 'hash2')
+bitcoin.createNewTransaction(200, "Sender2", "Recipient2");
+bitcoin.createNewTransaction(300, "Sender3", "Recipient3");
+bitcoin.createNewTransaction(400, "Sender4", "Recipient4");
+bitcoin.createNewBlock(300, 'prev3', 'hash3')
 
 console.log(bitcoin);
