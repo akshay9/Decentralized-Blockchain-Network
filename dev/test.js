4ef46beaa80f3e6075d71a40f02ca7f2be99cf45
hi_man_shoe <hello2himanshusingh@gmail.com>
2018-05-30 02:15:23 +0530

    Tested: HashBlock method

--- dev/test.js
+++ dev/test.js
@@ -4,12 +4,24 @@ const bitcoin = new Blockchain();
 /**
  * Testing the create block method with dummy data-set
  */
-bitcoin.createNewBlock(222, 'prev1', 'hash1')
-bitcoin.createNewTransaction(100, "Sender1", "Recipient1");
-bitcoin.createNewBlock(200, 'prev2', 'hash2')
-bitcoin.createNewTransaction(200, "Sender2", "Recipient2");
-bitcoin.createNewTransaction(300, "Sender3", "Recipient3");
-bitcoin.createNewTransaction(400, "Sender4", "Recipient4");
-bitcoin.createNewBlock(300, 'prev3', 'hash3')
+const previousBlockHash = "prevBlockHash";
+const currentBlockData = [
+    {
+        amount: 10,
+        sender: 'sender1',
+        recipient: 'recipient1'
+    },
+    {
+        amount: 20,
+        sender: 'sender2',
+        recipient: 'recipient2'
+    },
+    {
+        amount: 30,
+        sender: 'sender3',
+        recipient: 'recipient3'
+    },
+];
+const nonce = 100;
 
-console.log(bitcoin);
+console.log(bitcoin.hashBlock(previousBlockHash,currentBlockData,nonce));
