e9bf471a97c3177f148720920d56a01ae50f7a2e
hi_man_shoe <hello2himanshusingh@gmail.com>
2018-05-30 17:16:59 +0530

    Added : Body Parser

--- dev/api.js
+++ dev/api.js
@@ -1,8 +1,33 @@
 var express = require('express');
 var app = express();
-var port = 2000;
-app.get('/',function(req,res){
-    res.send("hello World");
+const bodyParser = require('body-parser')
+// this checks for the json format response first
+app.use(bodyParser.json);
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+var port = 4000;
+
+app.get('/blockchain', function (req, res) {
+    /**
+     * It will return all blockchain nodes
+     * 
+     */
+
+});
+app.post('/transaction', function (req, res) {
+    /**
+     * this will create a new transaction on blockchain
+     */
+
+});
+app.get('/mine', function (req, res) {
+    /**
+     * it will mine/create a new block for us
+     */
 });
 
-app.listen(port);
+
+app.listen(port, function () {
+    console.log('Listerning in ' + port);
+});
